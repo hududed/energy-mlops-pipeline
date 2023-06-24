66ce525364017634fcc2153106f3ce6d5a9ed9bc
Hud Wahab <hudwahab@gmail.com>
2023-06-24 08:40:04 -0600

    add settings utils

---
+++ feature-pipeline/feature_pipeline/settings.py
@@ -0,0 +1,31 @@
+import os
+from pathlib import Path
+from typing import Union
+
+from dotenv import load_dotenv
+
+
+def load_env_vars(root_dir: Union[str, Path]) -> dict:
+    """
+    Load environment variables from .env.default and .env files.
+    Args:
+        root_dir: Root directory of the .env files.
+    Returns:
+        Dictionary with the environment variables.
+    """
+    if isinstance(root_dir, str):
+        root_dir = Path(root_dir)
+    load_dotenv(dotenv_path=root_dir / ".env.default")
+    load_dotenv(dotenv_path=root_dir / ".env", override=True)
+    return dict(os.environ)
+
+
+CREDENTIALS = load_env_vars(root_dir="..")
+# TODO: Change output dir with a tmp dir that is deleted at the end of the training script + moved into a global location.
+OUTPUT_DIR = Path("output")
+# TODO: Find how to properly inject the root dit.
+# ROOT_DIR = Path("..")
+ROOT_DIR = Path("/opt/airflow/dags")
+CREDENTIALS = load_env_vars(root_dir=ROOT_DIR)
+OUTPUT_DIR = ROOT_DIR / "output"
+OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
